3b98cefcb3838ddde76342ecf790652d38ae0b21
rogerin07 <rogersantosbezerra@gmail.com>
2025-06-05 17:32:00 -0300

    criptografia

--- backend/domains/users/routes.js
+++ backend/domains/users/routes.js
@@ -8,7 +8,7 @@ const router = Router();
 const bcryptSalt = bcrypt.genSaltSync();
 
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
     connectDb();
 
     try {
@@ -36,6 +36,27 @@ router.post("/", async (req, res) => {
         });
 
         res.json(newUserDoc);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    connectDb();
+
+    const { email, password } = req.body;
+
+    try {
+        const userDoc = await User.findOne({ email });
+
+        if (userDoc) {
+            const passwordCorrect = bcrypt.compareSync(password, userDoc.password);
+            const { name, _id } = userDoc;
+
+            passwordCorrect ? res.json({ name, email, _id }) : res.status(400).json("senha inválida");
+        } else {
+            res.status(400).json("usuário não encontrado");
+        }
     } catch (error) {
         res.status(500).json(error);
     }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(
+      `Enviaram as seguintes informações. E-mail: ${email}. Senha:${password}`,
+    );
+  };
+
   return (
     <section className="flex items-center">
       <div className="mx-auto flex w-full max-w-96 flex-col items-center gap-4">
         <h1 className="text-3xl font-bold">Faça seu Login</h1>
 
-        <form className="flex w-full flex-col gap-2">
+        <form className="flex w-full flex-col gap-2" onSubmit={handleSubmit}>
           <input
             type="email"
             className="w-full rounded-full border border-gray-300 px-4 py-2"
             placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             className="w-full rounded-full border border-gray-300 px-4 py-2"
             placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <button className="w-full cursor-pointer rounded-full border border-gray-300 bg-green-400 px-4 py-2 font-bold text-white">
             Login
